36d1aa80048cc0ee4de94de48375c159e063f231
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2012-12-17 09:56:03 +0000

    Check changes once all packages are marked for modification

--- usr/lib/linuxmint/mintUpdate/checkWarnings.py
+++ usr/lib/linuxmint/mintUpdate/checkWarnings.py
@@ -24,15 +24,16 @@ try:
         for package in selection: 
             pkg = cache[package]
             pkg.mark_upgrade()
-            changes = cache.get_changes()
-            for pkg in changes:
-                if (pkg.is_installed):
-                    if (pkg.candidate.version == pkg.installed.version):
-                        if not pkg in packages_to_remove:
-                            packages_to_remove.append(pkg)
-                else:
-                    if not pkg in packages_to_install:
-                        packages_to_install.append(pkg)
+
+        changes = cache.get_changes()
+        for pkg in changes:
+            if (pkg.is_installed):
+                if (pkg.candidate.version == pkg.installed.version):
+                    if not pkg in packages_to_remove:
+                        packages_to_remove.append(pkg)
+            else:
+                if not pkg in packages_to_install:
+                    packages_to_install.append(pkg)
 
         installations = ' '.join(pkg.name for pkg in packages_to_install)
         removals = ' '.join(pkg.name for pkg in packages_to_remove)
