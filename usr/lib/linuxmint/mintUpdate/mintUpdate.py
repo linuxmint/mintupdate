6ce045bab537f5e84fbde4ce6143400abdc99a5c
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2014-12-04 11:48:32 +0100

    Don't force Mint originated updates to level 1, submit them to rules as well.

--- usr/lib/linuxmint/mintUpdate/mintUpdate.py
+++ usr/lib/linuxmint/mintUpdate/mintUpdate.py
@@ -677,44 +677,44 @@ class RefreshThread(threading.Thread):
                             extraInfo = ""
                             warning = ""
                             if is_a_mint_package:
-                                level = 1
+                                level = 1 # Level 1 by default
                             else:
-                                level = 3 # Level 3 by default                            
-                                rulesFile = open("/usr/lib/linuxmint/mintUpdate/rules","r")
-                                rules = rulesFile.readlines()
-                                goOn = True
-                                foundPackageRule = False # whether we found a rule with the exact package name or not
-                                for rule in rules:
-                                    if (goOn == True):
-                                        rule_fields = rule.split("|")
-                                        if (len(rule_fields) == 5):
-                                            rule_package = rule_fields[0]
-                                            rule_version = rule_fields[1]
-                                            rule_level = rule_fields[2]
-                                            rule_extraInfo = rule_fields[3]
-                                            rule_warning = rule_fields[4]
-                                            if (rule_package == source_package):
-                                                foundPackageRule = True
-                                                if (rule_version == newVersion):
+                                level = 3 # Level 3 by default
+                            rulesFile = open("/usr/lib/linuxmint/mintUpdate/rules","r")
+                            rules = rulesFile.readlines()
+                            goOn = True
+                            foundPackageRule = False # whether we found a rule with the exact package name or not
+                            for rule in rules:
+                                if (goOn == True):
+                                    rule_fields = rule.split("|")
+                                    if (len(rule_fields) == 5):
+                                        rule_package = rule_fields[0]
+                                        rule_version = rule_fields[1]
+                                        rule_level = rule_fields[2]
+                                        rule_extraInfo = rule_fields[3]
+                                        rule_warning = rule_fields[4]
+                                        if (rule_package == source_package):
+                                            foundPackageRule = True
+                                            if (rule_version == newVersion):
+                                                level = rule_level
+                                                extraInfo = rule_extraInfo
+                                                warning = rule_warning
+                                                goOn = False # We found a rule with the exact package name and version, no need to look elsewhere
+                                            else:
+                                                if (rule_version == "*"):
                                                     level = rule_level
                                                     extraInfo = rule_extraInfo
                                                     warning = rule_warning
-                                                    goOn = False # We found a rule with the exact package name and version, no need to look elsewhere
-                                                else:
-                                                    if (rule_version == "*"):
-                                                        level = rule_level
-                                                        extraInfo = rule_extraInfo
-                                                        warning = rule_warning
-                                            else:
-                                                if (rule_package.startswith("*")):
-                                                    keyword = rule_package.replace("*", "")
-                                                    index = source_package.find(keyword)
-                                                    if (index > -1 and foundPackageRule == False):
-                                                        level = rule_level
-                                                        extraInfo = rule_extraInfo
-                                                        warning = rule_warning
-                                rulesFile.close()
-                                level = int(level)
+                                        else:
+                                            if (rule_package.startswith("*")):
+                                                keyword = rule_package.replace("*", "")
+                                                index = source_package.find(keyword)
+                                                if (index > -1 and foundPackageRule == False):
+                                                    level = rule_level
+                                                    extraInfo = rule_extraInfo
+                                                    warning = rule_warning
+                            rulesFile.close()
+                            level = int(level)
 
                             # Create a new Update
                             update = PackageUpdate(source_package, level, oldVersion, newVersion, extraInfo, warning, update_type, tooltip)
