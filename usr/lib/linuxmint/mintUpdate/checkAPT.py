753a01c2a2d5b18a1e18dd7f484e05a3119262bb
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2016-12-07 12:27:22 +0000

    Kernel updates: Don't always recommend the "recommended" kernel.
    Only recommend the recommended kernel if the active kernel's version is lower.
    
    If the version of the active kernel is higher than the recommended one,
    recommend the highest version available instead.

--- usr/lib/linuxmint/mintUpdate/checkAPT.py
+++ usr/lib/linuxmint/mintUpdate/checkAPT.py
@@ -4,6 +4,7 @@ import os
 import sys
 import apt
 import gi
+import platform
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gio, Gtk
@@ -11,6 +12,22 @@ from gi.repository import Gio, Gtk
 from aptdaemon import client
 from aptdaemon.errors import NotAuthorizedError, TransactionFailed
 from aptdaemon.gtk3widgets import AptErrorDialog, AptProgressDialog
+
+class KernelVersion():
+
+    def __init__(self, version):
+        self.version = version
+        version_array = self.version.replace("-", ".").split(".")
+        self.numeric_versions = []
+        for i in range(4):
+            element = version_array[i]
+            if len(element) == 1:
+                element = "00%s" % element
+            elif len(element) == 2:
+                element = "0%s" % element
+            self.numeric_versions.append(element)
+        self.numeric_representation = ".".join(self.numeric_versions)
+        self.std_version = "%s.%s.%s-%s" % (version_array[0], version_array[1], version_array[2], version_array[3])
 
 try:
     cache = apt.Cache()
@@ -78,18 +95,44 @@ try:
                 resultString = u"UPDATE###%s###%s###%s###%s###%s###%s###%s###%s###%s###%s---EOL---" % (package, newVersion, oldVersion, size, sourcePackage, update_type, update_origin, short_description, description, update_site)
                 print(resultString.encode('ascii', 'xmlcharrefreplace'))
 
-    if kernel_updates:
-        if 'linux-image-generic' in cache:
-            versions = cache['linux-image-generic'].candidate.version.split(".")
-            if len(versions) > 3:
-                version = "%s.%s.%s-%s" % (versions[0], versions[1], versions[2], versions[3])
-                for pkgname in ['linux-headers-VERSION', 'linux-headers-VERSION-generic', 'linux-image-VERSION-generic', 'linux-image-extra-VERSION-generic']:
-                    pkgname = pkgname.replace('VERSION', version)
-                    if pkgname in cache:
-                        pkg = cache[pkgname]
-                        if not pkg.is_installed:
-                            resultString = u"UPDATE###%s###%s###%s###%s###%s###%s###%s###%s###%s###%s---EOL---" % (pkgname, pkg.candidate.version, "", pkg.candidate.size, "linux", "kernel", "ubuntu", pkg.candidate.raw_description, pkg.candidate.description, "security.ubuntu.com")
-                            print(resultString.encode('ascii', 'xmlcharrefreplace'))
+    try:
+        if kernel_updates:
+
+            # Get the uname version
+            uname_kernel = KernelVersion(platform.release())
+
+            # Get the recommended version
+            if 'linux-image-generic' in cache:
+                recommended_kernel = KernelVersion(cache['linux-image-generic'].candidate.version)
+                if (uname_kernel.numeric_representation <= recommended_kernel.numeric_representation):
+                    for pkgname in ['linux-headers-VERSION', 'linux-headers-VERSION-generic', 'linux-image-VERSION-generic', 'linux-image-extra-VERSION-generic']:
+                        pkgname = pkgname.replace('VERSION', recommended_kernel.std_version)
+                        if pkgname in cache:
+                            pkg = cache[pkgname]
+                            if not pkg.is_installed:
+                                resultString = u"UPDATE###%s###%s###%s###%s###%s###%s###%s###%s###%s###%s---EOL---" % (pkgname, pkg.candidate.version, "", pkg.candidate.size, "linux", "kernel", "ubuntu", pkg.candidate.raw_description, pkg.candidate.description, "security.ubuntu.com")
+                                print(resultString.encode('ascii', 'xmlcharrefreplace'))
+                else:
+                    # We're using a branch which is more recent than the recommended one, so we should recommend the latest kernel
+                    max_kernel = uname_kernel
+                    for pkg in cache:
+                        package_name = pkg.name
+                        if (package_name.startswith("linux-image-3") or package_name.startswith("linux-image-4")) and package_name.endswith("-generic"):
+                            version = package_name.replace("linux-image-", "").replace("-generic", "")
+                            kernel = KernelVersion(version)
+                            if kernel.numeric_representation > max_kernel.numeric_representation:
+                                max_kernel = kernel
+                    if max_kernel.numeric_representation != uname_kernel.numeric_representation:
+                        for pkgname in ['linux-headers-VERSION', 'linux-headers-VERSION-generic', 'linux-image-VERSION-generic', 'linux-image-extra-VERSION-generic']:
+                            pkgname = pkgname.replace('VERSION', max_kernel.std_version)
+                            if pkgname in cache:
+                                pkg = cache[pkgname]
+                                if not pkg.is_installed:
+                                    resultString = u"UPDATE###%s###%s###%s###%s###%s###%s###%s###%s###%s###%s---EOL---" % (pkgname, pkg.candidate.version, "", pkg.candidate.size, "linux", "kernel", "ubuntu", pkg.candidate.raw_description, pkg.candidate.description, "security.ubuntu.com")
+                                    print(resultString.encode('ascii', 'xmlcharrefreplace'))
+
+    except Exception as e:
+        pass
 
 except Exception as error:
     print("CHECK_APT_ERROR---EOL---")
