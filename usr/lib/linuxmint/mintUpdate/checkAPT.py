307ac75193d0c6f59005a3170968b39e9ddea788
gm10 <alt.f1-9y5ocr5@yopmail.com>
2018-11-08 12:50:17 +0100

    don't ignore Ubuntu kernel meta packages

--- usr/lib/linuxmint/mintUpdate/checkAPT.py
+++ usr/lib/linuxmint/mintUpdate/checkAPT.py
@@ -118,7 +118,7 @@ class APTCheck():
                         meta = self.cache[meta_name]
                         if meta.is_installed:
                             meta_installed = True
-                            break
+                            return
 
                 # If no meta is installed, try to recommend one
                 if not meta_installed:
@@ -138,10 +138,9 @@ class APTCheck():
                 if self.settings.get_boolean("use-lowlatency-kernels"):
                     kernel_type = "-lowlatency"
                 for pkg in self.cache:
-                    package_name = pkg.name
-                    if (package_name.startswith("linux-image-3") or package_name.startswith("linux-image-4")) and package_name.endswith(kernel_type):
-                        version = package_name.replace("linux-image-", "").replace("-generic", "").replace("-lowlatency", "")
-                        kernel = KernelVersion(version)
+                    match = re.match(r'^(?:linux-image-)(?:unsigned-)?(\d.+?)' + kernel_type + '$', pkg.name)
+                    if match:
+                        kernel = KernelVersion(match.group(1))
                         if kernel.numeric_representation > max_kernel.numeric_representation and kernel.series == max_kernel.series:
                             max_kernel = kernel
                 if max_kernel.numeric_representation != uname_kernel.numeric_representation:
