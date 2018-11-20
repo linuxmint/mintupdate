286fbec029a982214a2388392d52550d2dea9616
gm10 <13855078+gm10@users.noreply.github.com>
2018-11-20 18:25:27 +0100

    Preferences and automation rework: - re-organized to a more logical and concise arrangement, - added checkbox to toggle the entire refresh schedule, - completely refactored automation:   - now freely extensible, more options can easily be added,   - automatic updates kept as a cron job for backwards compatibility     but the script has been updated for new activations,   - added automation options for regular autoremove and clean,   - prevent regular system shutdown while automation is running,   - added persistent logging and logrotation to the automation log
    Updated package dependencies
    
    Amended the test script

---
+++ usr/share/linuxmint/mintupdate/automation/autoremove.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -s /usr/share/linuxmint/mintupdate/automation/99-mintupdate-temporary.pkla /etc/polkit-1/localauthority/90-mandatory.d/99-mintupdate-temporary.pkla
+echo "\n-- Automatic Removal $(date):\n" >> /var/log/mintupdate.log
+systemd-inhibit --why="Performing autoremoval" --who="Update Manager" --what=shutdown --mode=block /usr/bin/apt-get autoremove --purge -y >> /var/log/mintupdate.log 2>&1
+rm -f /etc/polkit-1/localauthority/90-mandatory.d/99-mintupdate-temporary.pkla
