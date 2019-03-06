9815febd1f1ba35f8111b36b7472d321e31c9d83
gm10 <13855078+gm10@users.noreply.github.com>
2019-03-06 08:39:29 +0100

    Information window use new Logger.read function

--- usr/lib/linuxmint/mintUpdate/mintUpdate.py
+++ usr/lib/linuxmint/mintUpdate/mintUpdate.py
@@ -1703,6 +1703,14 @@ class MintUpdate():
 ######### INFORMATION SCREEN #########
 
     def open_information(self, widget):
+        if self.information_window_showing:
+            return
+        def destroy_window(widget):
+            self.logger.remove_hook()
+            self.information_window_showing = False
+            window.destroy()
+        def update_log(line):
+            textbuffer.insert(textbuffer.get_end_iter(), line)
         gladefile = "/usr/share/linuxmint/mintupdate/information.ui"
         builder = Gtk.Builder()
         builder.set_translation_domain("mintupdate")
@@ -1710,12 +1718,14 @@ class MintUpdate():
         window = builder.get_object("main_window")
         window.set_title(_("Information"))
         window.set_icon_name("mintupdate")
-        builder.get_object("close_button").connect("clicked", self.hide_window, window)
+        textbuffer = builder.get_object("log_textview").get_buffer()
+        window.connect("destroy", destroy_window)
+        builder.get_object("close_button").connect("clicked", destroy_window)
         builder.get_object("processid_label").set_text(str(os.getpid()))
+        textbuffer.set_text(self.logger.read())
         builder.get_object("log_filename").set_text(str(self.logger.log.name))
-        txtbuffer = Gtk.TextBuffer()
-        txtbuffer.set_text(subprocess.check_output("cat " + self.logger.log.name, shell = True).decode("utf-8"))
-        builder.get_object("log_textview").set_buffer(txtbuffer)
+        self.logger.set_hook(update_log)
+        self.information_window_showing = True
 
 ######### HISTORY SCREEN #########
 
