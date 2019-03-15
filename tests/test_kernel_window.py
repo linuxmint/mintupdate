7ba1e54e863895407ed9a371d5f6625fe06d130b
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2019-03-15 12:44:54 +0000

    Add CI and dummy test

---
+++ tests/test_kernel_window.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../usr/lib/linuxmint/mintUpdate/')
+from Classes import PRIORITY_UPDATES
+
+
+def test_always_fail():
+    assert 3 == 5
+
+def test_always_succeeds():
+    assert 3 == 3
