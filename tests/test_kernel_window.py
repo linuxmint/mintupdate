4bb01f76f2323743f151c5b5eee676121c05431f
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2020-06-06 15:04:01 +0100

    tests: Remove call to obsolete get_eol_status()

--- tests/test_kernel_window.py
+++ tests/test_kernel_window.py
@@ -9,13 +9,6 @@ from Classes import *
 from mintUpdate import *
 from checkAPT import *
 
-# Test if the distribution EOL date is found
-def test_eol_is_found():
-    (is_eol, show_eol_warning, eol_date) = RefreshThread.get_eol_status()
-    assert isinstance(eol_date, datetime)
-    assert eol_date
-    assert eol_date.year > 2010
-
 # Test KernelVersion object
 def test_kernel_version_series_comparison():
     version1 = KernelVersion ("4.8.0-2-generic")
