e08bedfd56d4b679e04d4f59feaac3cc3dd5704b
linux-mythic-archive <adam.mikenones.levi@outlook.com>
2026-01-23 11:04:44 +0100

    Update Classes.py

--- usr/lib/MythicOS/mythic-update/Classes.py
+++ usr/lib/MythicOS/mythic-update/Classes.py
@@ -6,7 +6,7 @@ import urllib.request
 
 APP_NAME = "Gestionnaire de mise à jour"
 
-# Dépôt MythicOS (OFFICIEL)
+# Dépôt MythicOS
 MYTHIC_REPO_LINE = (
     "deb [signed-by=/usr/share/keyrings/mythicos.gpg] "
     "https://packages.mythicos.hastag.fr/ stable main\n"
@@ -14,9 +14,16 @@ MYTHIC_REPO_LINE = (
 
 MYTHIC_LIST_FILE = "/etc/apt/sources.list.d/mythicos.list"
 
-# Clé GPG MythicOS (telle quelle)
+# Clé GPG
 MYTHIC_KEY_URL = "https://packages.mythicos.hastag.fr/mythicos.gpg"
 MYTHIC_KEY_PATH = "/usr/share/keyrings/mythicos.gpg"
+
+# APT pinning
+MYTHIC_PIN_FILE = "/etc/apt/preferences.d/mythicos.pref"
+MYTHIC_PIN_CONTENT = """Package: *
+Pin: origin packages.mythicos.hastag.fr
+Pin-Priority: 1001
+"""
 
 
 class RepositoryManager:
@@ -24,13 +31,13 @@ class RepositoryManager:
     def ensure_repositories(self):
         self.ensure_key()
         self.ensure_repo()
+        self.ensure_pinning()
 
     def ensure_key(self):
         if os.path.exists(MYTHIC_KEY_PATH):
             return
 
         print("Téléchargement de la clé GPG MythicOS…")
-
         os.makedirs("/usr/share/keyrings", exist_ok=True)
         urllib.request.urlretrieve(MYTHIC_KEY_URL, MYTHIC_KEY_PATH)
         os.chmod(MYTHIC_KEY_PATH, 0o644)
@@ -40,6 +47,13 @@ class RepositoryManager:
             return
 
         print("Ajout du dépôt MythicOS…")
-
         with open(MYTHIC_LIST_FILE, "w", encoding="utf-8") as f:
             f.write(MYTHIC_REPO_LINE)
+
+    def ensure_pinning(self):
+        if os.path.exists(MYTHIC_PIN_FILE):
+            return
+
+        print("Application de la priorité APT MythicOS…")
+        with open(MYTHIC_PIN_FILE, "w", encoding="utf-8") as f:
+            f.write(MYTHIC_PIN_CONTENT)
